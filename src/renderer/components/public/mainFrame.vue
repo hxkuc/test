c615c4c323de0478dd20fc2d9d1ae0d30cda2f36
huang <2374266244@qq.com>
2018-06-16 17:23:02 +0800

    add background

--- src/renderer/components/public/mainFrame.vue
+++ src/renderer/components/public/mainFrame.vue
@@ -1,76 +1,84 @@
 <template>
-    <div class="fullWindow">
-    	<div class="backgroundclass"></div>
+    <div class="fullWindow" :style="fontColor">
+      <div class="backgroundclass" :style="backGroundParent">
+    	<div class="backBodyClass" :style="backGroundChild"></div>
+      </div>
         <header class="headDiv">
-        	<slot name="header"></slot>
+          <slot name="header"></slot>
         </header>
         <main class="bodyDiv">
-			<slot>
-		      <transition name="default">
-		        <router-view></router-view>
-		      </transition>
-		    </slot>
+      <slot>
+          <transition name="default">
+            <router-view></router-view>
+          </transition>
+        </slot>
         </main>
         <footer class="footDiv">
-			<slot name="footer"></slot>
+      <slot name="footer"></slot>
         </footer>
     </div>
 </template>
 <script>
+import { mapGetters } from 'vuex'
 export default {
   name: 'mainFrame',
-  methods: {
+  computed: {
+    ...mapGetters([
+      'backGroundParent',
+      'backGroundChild',
+      'fontColor'
+    ])
   }
 }
 </script>
 <style scoped>
 .fullWindow {
-	color: #fff;
-	border-radius: 3px;
-	width: 100%;
-	height: 100%;
-	display: flex;
-	flex-direction: column;
-	box-shadow: 0 1px 6px 1px rgba(0, 0, 0, 0.3);
-	justify-content: space-between;
-	overflow: hidden;
-	position: relative;
+  border-radius: 3px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0 1px 6px 1px rgba(0, 0, 0, 0.3);
+  justify-content: space-between;
+  overflow: hidden;
+  position: relative;
 }
 .bodyDiv {
-	width:100%;
-	height: 100%;
-	overflow-y: auto;
-	word-break: break-all;
-	flex-grow: 1;
+  width:100%;
+  height: 100%;
+  overflow-y: auto;
+  word-break: break-all;
+  flex-grow: 1;
 }
 .headDiv {
-	flex-shrink: 0;
-	-webkit-app-region: drag;
+  flex-shrink: 0;
+  -webkit-app-region: drag;
 }
 .footDiv {
-	flex-shrink: 0;
+  flex-shrink: 0;
 }
 .backgroundclass{
-	z-index: -1;
-	position: absolute;
-	background: url(./../../../../static/123.jpg) center center no-repeat fixed;
-	-webkit-background-size:cover;
-    -moz-background-size:cover;
-    background-size:cover;
-	width: 100%;
-	height: 100%;
-	overflow: hidden;
-	box-sizing: border-box;
+  z-index: -1;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  box-sizing: border-box;
 }
-
-.backgroundclass:after{
+/* .backgroundclass::after{
     content: "";
     width:100%;
     height:100%;
     position: absolute;
     background: inherit;
-    filter: blur(10px);
+    filter: attr(data-attr);
     overflow: hidden;
     box-sizing: border-box;
+} */
+.backBodyClass{
+  width:100%;
+  height:100%;
+  overflow: hidden;
+  box-sizing: border-box;
 }
 </style>
--- src/renderer/store/modules/BaseConfig.js
+++ src/renderer/store/modules/BaseConfig.js
@@ -1,10 +1,58 @@
 const state = {
-  trransition: 'default'
+  trransition: 'default',
+  backGroundType: 1,
+  backGroundBlur: 2,
+  backGroundImg: '123.jpg',
+  backGroundColor: '#000',
+  fontColor: '#fff'
+}
+
+const getters = {
+  backGroundParent: state => {
+    if (state.backGroundType === 1) {
+      return {
+        'background': 'url("static/background/' + state.backGroundImg + '") center center / cover no-repeat fixed'
+      }
+    } else {
+      return {
+        'background': state.backGroundColor
+      }
+    }
+  },
+  backGroundChild: state => {
+    if (state.backGroundType === 1) {
+      return {
+        filter: 'blur(' + state.backGroundBlur + 'px)',
+        background: 'inherit'
+      }
+    } else {
+      return {}
+    }
+  },
+  fontColor: state => {
+    return {
+      color: state.fontColor
+    }
+  }
 }
 
 const mutations = {
   CHANGE_TRANSITION (state, type) {
     state.trransition = type
+  },
+  CHANGE_BACKGROUNDBLUR (state, type) {
+    state.backGroundBlur = type
+  },
+  CHANGE_COLOR (state, type) {
+    state.backGroundType = 2
+    state.backGroundColor = type
+  },
+  CHANGE_FONTCOLOR (state, type) {
+    state.fontColor = type
+  },
+  CHANGE_BACKGROUNDIMG (state, type) {
+    state.backGroundType = 1
+    state.backGroundImg = type
   }
 }
 
@@ -16,6 +64,7 @@ const actions = {
 
 export default {
   state,
+  getters,
   mutations,
   actions
 }
